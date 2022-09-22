d8679d63ddcd58c1455606f90e6a1289e4a2fcc9
Brian James <brianpja@gmail.com>
2022-09-22 17:14:18 -0400

    created new customer component

---
+++ src/components/Customer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface IProps {
+
+};
+
+let Customer:React.FC<IProps> = () => {
+  return(
+		<>
+			<h2>Customer Component</h2>
+		</>
+	)
+};
+
+export default Customer;
