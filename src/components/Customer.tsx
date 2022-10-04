13286adba8df12062cab3762ee410aa2fc527722
Brian James <brianpja@gmail.com>
2022-10-04 13:19:48 -0400

    editing name and age

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 interface Props {
 	name: string;
 	age?: number;
 };
 
+interface CustomerData {
+	name: string,
+	age?: number
+}
+
 const Customer = ({name, age}: Props) => {
 
+	const [editMode, setEditMode] = useState<boolean> (false);
+	const [customerData, setCustomerData] = useState<CustomerData> ({
+		name,
+		age
+	});
+
+	const toggleEditMode = (): void => {
+		setEditMode(!editMode);
+	}
+
+	const updateValue = (e: ChangeEvent<HTMLInputElement>, fieldName: string): any => {
+		const newData = { ...customerData, [fieldName]: e.target?.value };
+		setCustomerData(newData);
+	};
 	
 	return(
 		<div className="customer">
 			<h2>Customer </h2>
-			<div>Name: {name}{age ? `, Age: ${age}` : ''}</div>
+			<div>
+				{
+					editMode ? (
+						<>
+							<input value={customerData.name} onChange={(e: ChangeEvent<HTMLInputElement>) => updateValue(e, 'name')} />
+							<input value={customerData.age} onChange={(e: ChangeEvent<HTMLInputElement>) => updateValue(e, 'age')} />
+						</>
+					) :
+					(
+						<>
+							<span>Name: {customerData.name}</span>
+							<span>{customerData.age ? `, Age: ${customerData.age}` : ''}</span>
+						</>
+					)
+				}
+				
+			</div>
+			<button onClick={toggleEditMode}>{ editMode ? 'Save' : 'Edit' }</button>
+			{
+				editMode && <div>Edit Mode</div>
+			}
 		</div>
 	)
 };
